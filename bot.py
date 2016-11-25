b44e2d24648b7b7e8e6673f520df93d31013c43e
belovanas <belovanas21@mail.ru>
2016-11-25 21:44:29 +0300

    added say_hello
    Signed-off-by: belovanas <belovanas21@mail.ru>

--- bot.py
+++ bot.py
@@ -4,4 +4,11 @@ import telebot
 bot = telebot.TeleBot(config.token)
 #fjtylui'
 #Обработчик команд start и end
-#@bot.message_handler()
+@bot.message_handler(content_types=["text"])
+def say_hello(message):
+    bot.send_message(message.chat.id, "Hello, guy")
+#def say_bye(message):
+    #if
+
+if __name__ == '__main__':
+    bot.polling(none_stop=True)
