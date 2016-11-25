6ff746b8c75bb55eddbe298bec2f2382504dccdb
belovanas <belovanas21@mail.ru>
2016-11-25 22:01:01 +0300

    added say_bye
    Signed-off-by: belovanas <belovanas21@mail.ru>

--- bot.py
+++ bot.py
@@ -4,11 +4,12 @@ import telebot
 bot = telebot.TeleBot(config.token)
 #fjtylui'
 #Обработчик команд start и end
-@bot.message_handler(content_types=["text"])
+@bot.message_handler(commands=['start'])
 def say_hello(message):
-    bot.send_message(message.chat.id, "Hello, guy")
-#def say_bye(message):
-    #if
+    bot.send_message(message.chat.id, "Hello, guy!")
+@bot.message_handler(commands=['finish'])
+def say_bye(message):
+    bot.send_message(message.chat.id, "Bye, friend!")
 
 if __name__ == '__main__':
     bot.polling(none_stop=True)
