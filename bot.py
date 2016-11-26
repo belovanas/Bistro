f3db2d3d67f6ac0f0d77cef3cf12dc6348d64b77
belovanas <belovanas21@mail.ru>
2016-11-26 15:40:59 +0300

    added signup
    Signed-off-by: belovanas <belovanas21@mail.ru>

--- bot.py
+++ bot.py
@@ -6,21 +6,22 @@ bot = telebot.TeleBot(config.token)
 #Обработчик команд start и end
 @bot.message_handler(commands=['start'])
 def say_hello(message):
-    bot.send_message(message.chat.id, "Hello, guy!")
+    bot.send_message(message.chat.id, "Привет)")
 
 @bot.message_handler(commands=['finish'])
 def say_bye(message):
-    bot.send_message(message.chat.id, "Bye, friend!")
+    bot.send_message(message.chat.id, "Пока!")
     bot.leave_chat(message.chat.id)
 
 #Обработчик команды SignUp
 @bot.message_handler(commands=['signup'])
 def invitation_to_registration(message):
-    bot.send_message(message.chat.id, "Please, enter your name: ")
+    bot.send_message(message.chat.id, "Пожалуйста, введите свое имя: ")
 
 @bot.message_handler(content_types=["text"])
 def catch_name(message):
     name = message.text
+    bot.send_message(message.chat.id, text=name)
 
 if __name__ == '__main__':
     bot.polling(none_stop=True)
